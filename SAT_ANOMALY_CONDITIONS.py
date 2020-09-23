ef96dccbc711d2b792dc4f0ff31ca9fd083e8052
m-atoms <m.adams@pitt.edu>
2020-09-23 05:42:56 -0700

    add limit occurrence thresholds for anomaly detection

---
+++ SAT_ANOMALY_CONDITIONS.py
@@ -0,0 +1,15 @@
+################################################################
+#                                                              #
+#  Satellite Telemetry Anomaly Conditions                      #
+#                                                              #
+#  note: Acceptable rate of occurrence for limit conditions     #
+#        was chosen based on the results of sat_limit.         #
+#        Significantly more background info and system testing #
+#        would be required to set these anomaly detection      #
+#        thresholds in practice.                               #
+#                                                              #
+################################################################
+
+WHEEL_SATURATION_LIMIT = 0.055
+GYRO_SPIN_LIMIT = 4.78
+LOW_VOLTAGE_LIMIT = 0
